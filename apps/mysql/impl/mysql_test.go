64f10a60b68a7b90829ef5a4c2d33af55f65ae6b
solodba <945623397@qq.com>
2023-08-23 16:51:03 +0800

    添加MySQL安装单元测试实例

--- apps/mysql/impl/mysql_test.go
+++ apps/mysql/impl/mysql_test.go
@@ -8,3 +8,31 @@ func TestUnzipMySQLFile(t *testing.T) {
 		t.Log(err)
 	}
 }
+
+func TestCreateMySQLDir(t *testing.T) {
+	err := svc.CreateMySQLDir(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCreateMySQLUser(t *testing.T) {
+	err := svc.CreateMySQLUser(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestChangeMySQLDirPerm(t *testing.T) {
+	err := svc.ChangeMySQLDirPerm(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestInitialMySQL(t *testing.T) {
+	err := svc.InitialMySQL(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
