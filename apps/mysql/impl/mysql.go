adbddba60ad368049a6a4768f8776570683962bc
code-horse-mi <945623397@qq.com>
2023-08-23 10:53:53 +0800

    添加解压MySQL压缩包功能

--- apps/mysql/impl/mysql.go
+++ apps/mysql/impl/mysql.go
@@ -1,9 +1,40 @@
 package impl
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/solodba/mcube/logger"
+)
 
 // 解压MySQL压缩文件
 func (i *impl) UnzipMySQLFile(ctx context.Context) error {
+	_, err := os.Stat(i.c.MySQL.InstallPath)
+	if err == nil {
+		return fmt.Errorf("[%s]文件夹已经存在,请确定是否安装了MySQL", i.c.MySQL.InstallPath)
+	}
+	logger.L().Info().Msgf("正在解压MySQL压缩包")
+	cmd := exec.Command("xd", "-d", i.c.MySQL.FileName)
+	_, err = cmd.Output()
+	if err != nil {
+		return fmt.Errorf("解压MySQL压程序失败: %v", err.Error())
+	}
+	tarName := strings.TrimRight(i.c.MySQL.FileName, ".xz")
+	cmd = exec.Command("tar", "xf", tarName)
+	_, err = cmd.Output()
+	if err != nil {
+		return fmt.Errorf("解压MySQL压程序失败: %v", err.Error())
+	}
+	dirName := strings.TrimRight(tarName, ".tar")
+	cmd = exec.Command("mv", dirName, i.c.MySQL.InstallPath)
+	_, err = cmd.Output()
+	if err != nil {
+		return fmt.Errorf("移动MySQL解压包失败: %v", err.Error())
+	}
+	logger.L().Info().Msgf("解压MySQL压缩包成功")
 	return nil
 }
 
